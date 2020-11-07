675d694ac04814a928b04c4d2c13d785ba1fca0a
Ivan <oiv@live.ru>
2020-11-08 03:01:51 +0500

    ver. 1.4 Реализован троттлинг средствами DRF

--- orders/settings.py
+++ orders/settings.py
@@ -123,6 +123,16 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'market.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '15/minute',
+        'user': '1/second'
+    }
+}
 
 # Настройки для отправки почты
 
